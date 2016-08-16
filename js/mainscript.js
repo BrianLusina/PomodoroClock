aadbfceb0f4409bb01f4b0742687bf88a4aa1b3c
Brian Lusina <lusinabrian@gmail.com>
2016-08-16 11:41:09 +0300

    Add format time function to format the time based on the session length

--- js/mainscript.js
+++ js/mainscript.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
-	//TODO: insert functions
+	var sessionLength = 25, 
+		breakLength = 5,
+		isSession = true,
+		timeLeft = sessionLength * 60,
+		isPauses = true;
+	
+	var fomartTime = function(){
+		var foo = Math.floor(timeLeft / 60);
+		var bar = timeLeft - foo * 60;
+		return foo + ":" + (bar < 10 ? "0" : "") + bar;
+	};
 
 });
