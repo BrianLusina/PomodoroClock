43674506e074efbdc10718f336737ab8d2d45894
Brian Lusina <lusinabrian@gmail.com>
2016-08-16 11:45:29 +0300

    Add Update Session Settings
    Function update Settings for Pomodoro Clock, How long break will be and session length

--- js/mainscript.js
+++ js/mainscript.js
@@ -4,11 +4,21 @@ $(document).ready(function(){
 		isSession = true,
 		timeLeft = sessionLength * 60,
 		isPauses = true;
-	
+
+	/*function to format time*/
 	var fomartTime = function(){
 		var foo = Math.floor(timeLeft / 60);
 		var bar = timeLeft - foo * 60;
 		return foo + ":" + (bar < 10 ? "0" : "") + bar;
 	};
+	
+	var fillAnimationSess = null,
+ 		fillAnimationBreak = null,
+		intervalCounter = null;
+	
+	/*function to update Session settings*/
+	var updateSettingsTimeValues = function() {
+      $('.Settings>.break>.value>.v').text(breakLength); $('.Settings>.session>.value>.v').text(sessionLength);
+    }
 
 });
